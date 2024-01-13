6aa3468afb65991ea4b28606f87c474f536e109f
qqrm <qqrm@mail.ru>
2024-01-14 03:54:02 +0500

    Add rebuilt script

---
+++ rebuild.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+# Fetch the latest changes from the remote repository
+git fetch origin
+
+# Reset local main branch to match the remote main branch, discarding local changes
+git reset --hard origin/main
+
+# Optionally, clean up untracked files
+git clean -fd
+
+# Stop and remove the existing container if it exists
+docker stop my_shitverter_container || true
+docker rm my_shitverter_container || true
+
+# Build the Docker image with the tag 'shitverter:latest'
+docker build -t shitverter:latest .
+
+# Run the new container here
+docker run -d -e TELEGRAM_API_TOKEN=$TELEGRAM_API_TOKEN --name my_shitverter_container shitverter:latest
+
