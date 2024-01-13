a910c181fad7ba59ee0e13cd179009577a24be16
qqrm <qqrm@mail.ru>
2024-01-14 03:14:32 +0500

    Refactor, ID's for new members
    - Added new functionality to process and respond ID's to new chat member joins.
    - Little Refactor

--- src/main.rs
+++ src/main.rs
@@ -20,38 +20,56 @@ async fn main() {
 
     // Start the bot and handle incoming messages.
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        // Ensure the message is of a common type (text, media, etc.).
-        let MessageKind::Common(common) = &msg.kind else {
-            return respond(());
-        };
-
-        // Ensure the media in the message is a document.
-        let MediaKind::Document(document) = &common.media_kind else {
-            return respond(());
-        };
-
-        // Check if the document has a MIME type and retrieve it.
-        let Some(mime_type) = &document.document.mime_type else {
-            return respond(());
-        };
-
-        // Check if the MIME type of the document is 'video/webm'.
-        if mime_type.essence_str() != "video/webm" {
-            return respond(());
-        };
-
         // Process the webm file and handle the result.
-        match process_webm(&bot, &document.document.file.id, &msg).await {
-            Ok(_) => {
-                log::info!("Processed webm file")
-            }
-            Err(e) => {
-                log::error!("Error processing webm file: {:?}", e)
-            }
-        }
+        let _ = process_webm(&bot, &msg)
+            .await
+            .map_err(|e| log::error!("Error processing webm file: {:?}", e));
+
+        // Process new members and handle the result.
+        let _ = process_new_member(&bot, &msg)
+            .await
+            .map_err(|e| log::error!("Error processing webm file: {:?}", e));
+
         respond(())
     })
     .await;
+}
+
+/// Processes the joining of a new member and sends a message with their IDs.
+///
+/// # Arguments
+///
+/// * `bot` - Reference to the Telegram Bot instance.
+/// * `msg` - Reference to the incoming Telegram Message.
+///
+/// # Returns
+///
+/// A `Result` indicating the success or failure of the processing.
+async fn process_new_member(bot: &Bot, msg: &Message) -> Result<(), Box<dyn std::error::Error>> {
+    // Ensure the message is of a joining type
+    let MessageKind::NewChatMembers(new_members_msg) = &msg.kind else {
+        return Ok(());
+    };
+
+    // Build resp with ID's
+    let resp_with_ids: String = new_members_msg
+    .new_chat_members
+    .iter()
+    .map(|user| {
+        format!(
+            "Check ASAP [{}](tg://user?id={}) with id {}\n",
+            user.full_name(),
+            user.id,
+            user.id
+        )
+    })
+    .collect();
+
+    // Send response
+    bot.send_message(msg.chat.id, resp_with_ids)
+    .parse_mode(ParseMode::MarkdownV2).await?;
+
+    Ok(())
 }
 
 /// Processes a `.webm` file by converting it to `.mp4` and sends it back via Telegram.
@@ -59,19 +77,33 @@ async fn main() {
 /// # Arguments
 ///
 /// * `bot` - Reference to the Telegram Bot instance.
-/// * `file_id` - The unique file identifier for the `.webm` file.
-/// * `chat_id` - The chat ID where the message originated.
-/// * `message_id` - The message ID of the original `.webm` file message.
+/// * `msg` - Reference to Message.
 ///
 /// # Returns
 ///
 /// A `Result` indicating the success or failure of the processing.
-async fn process_webm(
-    bot: &Bot,
-    file_id: &str,
-    msg: &Message,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let file_path = download_file(bot, file_id).await?;
+async fn process_webm(bot: &Bot, msg: &Message) -> Result<(), Box<dyn std::error::Error>> {
+    // Ensure the message is of a common type (text, media, etc.).
+    let MessageKind::Common(common) = &msg.kind else {
+        return Ok(());
+    };
+
+    // Ensure the media in the message is a document.
+    let MediaKind::Document(document) = &common.media_kind else {
+        return Ok(());
+    };
+
+    // Check if the document has a MIME type and retrieve it.
+    let Some(mime_type) = &document.document.mime_type else {
+        return Ok(());
+    };
+
+    // Check if the MIME type of the document is 'video/webm'.
+    if mime_type.essence_str() != "video/webm" {
+        return Ok(());
+    };
+
+    let file_path = download_file(bot, &document.document.file.id).await?;
     let converted_file_path = convert_webm_to_mp4(&file_path)?;
 
     let mut send_video_request = bot.send_video(msg.chat.id, InputFile::file(&converted_file_path));
@@ -84,20 +116,22 @@ async fn process_webm(
     if let Some(user) = msg.from() {
         let full_name = user.full_name();
         let signature = format!("send by [{}](tg://user?id={})", full_name, user.id);
-    
+
         let caption = msg.caption().map_or_else(
             || signature.clone(),
             |existing_caption| format!("{}\n\n{}", existing_caption, signature),
         );
-    
-        send_video_request = send_video_request.caption(caption).allow_sending_without_reply(true);
-    }
 
+        send_video_request = send_video_request
+            .caption(caption)
+            .allow_sending_without_reply(true);
+    }
 
     if let Some(msg) = msg.reply_to_message() {
         send_video_request = send_video_request.reply_to_message_id(msg.id);
     }
-    send_video_request.parse_mode(ParseMode::MarkdownV2).await?;
+    send_video_request = send_video_request.parse_mode(ParseMode::MarkdownV2);
+    send_video_request.await?;
     bot.delete_message(msg.chat.id, msg.id).await?;
 
     // Clean up: delete the downloaded and converted files
