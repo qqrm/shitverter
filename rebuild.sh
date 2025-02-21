5593b80e076d9733773cf8e310fa877d5a16bfed
Alexey <4681325+qqrm@users.noreply.github.com>
2025-02-21 14:19:23 +0300

    Update rebuild.sh
    fixup rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -1,20 +1,20 @@
 #!/bin/zsh
 
-# Fetch the latest changes from the remote repository
+# Получаем последние изменения с удалённого репозитория "origin"
 git fetch origin
 
-# Reset local main branch to match the remote main branch, discarding local changes
-git reset --hard origin/main
+# Сбрасываем локальную ветку, устанавливая её равной удалённой ветке "async" из "origin"
+git reset --hard origin/async
 
-# Optionally, clean up untracked files
+# Опционально, очищаем неотслеживаемые файлы
 git clean -fd
 
-# Stop and remove the existing container if it exists
+# Останавливаем и удаляем существующий контейнер, если он есть
 docker stop my_shitverter_container || true
 docker rm my_shitverter_container || true
 
-# Build the Docker image with the tag 'shitverter:latest'
+# Собираем Docker-образ с тегом 'shitverter:latest'
 docker build -t shitverter:latest .
 
-# Run the new container here
+# Запускаем новый контейнер, передавая переменную окружения TELOXIDE_TOKEN
 docker run -d -e TELOXIDE_TOKEN=$TELEGRAM_API_TOKEN --name my_shitverter_container shitverter:latest
